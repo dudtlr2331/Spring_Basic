4ffee6e04a168718ec2586b9c5f8e4b375b6b60f
Cat Hanbit <dudtlr102@naver.com>
2023-03-11 23:03:00 +0900

    임포트 수정

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,5 +1,14 @@
 package hello.core.scope;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 public class SingletonWithPrototypeTest1 {
     @Test
     void prototypeFind() {
@@ -7,22 +16,28 @@ public class SingletonWithPrototypeTest1 {
         PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
         prototypeBean1.addCount();
         assertThat(prototypeBean1.getCount()).isEqualTo(1);
+        
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
         prototypeBean2.addCount();
         assertThat(prototypeBean2.getCount()).isEqualTo(1);
     }
     @Scope("prototype")
-    static class PrototypeBean {private int count = 0;
+    static class PrototypeBean {
+        private int count = 0;
+
         public void addCount() {
             count++;
         }
+
         public int getCount() {
             return count;
         }
+
         @PostConstruct
         public void init() {
             System.out.println("PrototypeBean.init " + this);
         }
+
         @PreDestroy
         public void destroy() {
             System.out.println("PrototypeBean.destroy");
