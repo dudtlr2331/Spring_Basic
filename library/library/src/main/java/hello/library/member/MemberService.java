db1f598221070f505070dff21c0267701c6bea6c
dudtlr2331 <dudtlr102@naver.com>
2025-06-14 16:18:02 +0900

    LifeCycle 예제

--- library/library/src/main/java/hello/library/member/MemberService.java
+++ library/library/src/main/java/hello/library/member/MemberService.java
@@ -1,5 +1,7 @@
 package hello.library.member;
 
+import org.springframework.stereotype.Service;
+
 public interface MemberService {
     void join(Member member);
     Member findMember(Long memberId);
