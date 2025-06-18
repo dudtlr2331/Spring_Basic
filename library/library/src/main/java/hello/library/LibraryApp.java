3c535c0c6b64d97d63ed2dc053c6bcbc57053334
dudtlr2331 <dudtlr102@naver.com>
2025-06-18 23:58:27 +0900

    스터디 예제

--- library/library/src/main/java/hello/library/LibraryApp.java
+++ library/library/src/main/java/hello/library/LibraryApp.java
@@ -5,6 +5,7 @@ import hello.library.rental.Rental;
 import hello.library.rental.RentalService;
 import hello.library.rental.RentalServiceImpl;
 import hello.library.rental.VipRentalPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -16,14 +17,14 @@ public class LibraryApp {
 //        MemberService memberService = new MemberServiceImpl();
 //        RentalService rentalService = new RentalServiceImpl(new VipRentalPolicy(), new MemoryMemberRepository());
 
-//        AppConfig appConfig = new AppConfig();
-//        MemberService memberService = appConfig.memberService();
-//        RentalService rentalService = appConfig.rentalService();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        RentalService rentalService = appConfig.rentalService();
 
-//        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
-        MemberService memberService = ac.getBean(MemberService.class);
-        RentalService rentalService = ac.getBean(RentalService.class);
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+//        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+//        MemberService memberService = ac.getBean(MemberService.class);
+//        RentalService rentalService = ac.getBean(RentalService.class);
 
         Member member = new Member(1L, "Alice", Grade.VIP);
         memberService.join(member);
--- library/library/src/main/java/hello/library/member/MemberServiceImpl.java
+++ library/library/src/main/java/hello/library/member/MemberServiceImpl.java
@@ -2,6 +2,9 @@ package hello.library.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 @Component
 public class MemberServiceImpl implements MemberService {
